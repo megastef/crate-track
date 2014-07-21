547a2fc15c782208b371f6410aee6b962871a9c2
seti123 <seti123@aldebaran.uberspace.de>
2014-07-22 00:24:18 +0200

    added apache parser

---
+++ access_log_parser.js
@@ -0,0 +1,34 @@
+var fs = require('fs')
+var log = fs.readFileSync ('./logs/access_log')
+var regexLogLine = /^([0-9a-f.:]+) - - \[([0-9]{2}\/[a-z]{3}\/[0-9]{4}):([0-9]{2}:[0-9]{2}:[0-9]{2}[^\]]*)\] \"([^\"]+)\" [0-9]+ [0-9]+ (\"([^\"]+)\") (\"([^\"]+)\")/i;
+var regexHttpRequest = /^(GET|POST) (.+) HTTP\/(1.[0-1])$/i;
+var dtStart = new Date(0)
+var lines = log.toString().split ('\n')
+var objects = lines.map (function (e){
+	var parts = regexLogLine.exec(e);
+        if ( parts != null ) {
+            var recDate = Date.parse(new Date(parts[2]+' '+parts[3]));
+
+            // Determine the earliest datetime covered by log
+            if (recDate < dtStart ) {
+                dtStart = recDate;
+            }
+
+            // Process the HTTP request portion
+            var httpRec = regexHttpRequest.exec(parts[4]);
+            if ( httpRec != null ) {
+                return {
+		
+                    datetime: recDate,
+                    client: parts[1],
+		    method: httpRec[1],
+                    http: httpRec[3],
+                    uri: httpRec[2],
+		    referer: parts[6],
+		    useragent: parts[8]
+                }
+		return parts;
+            }
+        } 
+})
+console.log (objects)
